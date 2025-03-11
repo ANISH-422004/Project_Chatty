68baafe9483e6fd44ec11e91b8dff41080a3cb27
ANISH-422004 <anishbhattacharya422004@gmail.com>
2025-03-11 21:01:51 +0530

    Production

--- Backend/src/app.js
+++ Backend/src/app.js
@@ -5,6 +5,7 @@ const userRoutes = require('./routes/user.routes');
 const chatRoutes = require("./routes/chat.routes")
 const messageRoutes = require("./routes/message.routes")
 const app = express();
+const path = require("path")
 
 // Middleware setup
 app.use(cors());
@@ -19,6 +20,27 @@ app.get("/", (req, res) => {
 app.use('/api/v1/user', userRoutes);
 app.use('/api/v1/chat' , chatRoutes)
 app.use('/api/v1/message',messageRoutes)
+
+
+//-------------Deployment---------------
+
+const _dirname1 = path.resolve()
+if(process.env.NODE_ENV === 'production') {
+  app.use(express.static(path.join(_dirname1, 'FrontEnd', 'dist')))
+  app.get("*",(req,res)=>{
+    res.sendFile(_dirname1,"FrontEnd","dist","index.html")
+  })
+
+}else{
+  app.get("/",(req,res)=>{
+    res.send("API is Running Successfully")
+  })
+}
+
+//-------------Deployment---------------
+
+
+
 
 
 
